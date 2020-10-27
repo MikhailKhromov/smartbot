caf86b3972bc1e7e625a318169e62973c166f335
khrom <khromov05@gmail.com>
2020-10-27 18:39:22 +0300

    now its always possible to get from point A to point B

--- enviroment/grid.py
+++ enviroment/grid.py
@@ -15,9 +15,39 @@ class Grid:
     def generate_matrix(self):
         self.matrix = [[Node(self.get_random_coeff(i, j), i, j) for j in range(self.width)] for i in range(self.height)]
 
+    def convert_coords_to_id(self, coords):
+        return coords[0] * self.width + coords[1]
+
     def check_path(self, coords_from, coords_to):
-        pass
-        # Tries to find path from coords_from to coords_to
-        # dfs or something
+        items = []
+        def update_items():
+            if self.matrix[curr_coords[0]][curr_coords[1]].LEFT:
+                if visited.count((curr_coords[0], curr_coords[1] - 1)) == 0:
+                    items.append((curr_coords[0], curr_coords[1] - 1))
+            if self.matrix[curr_coords[0]][curr_coords[1]].TOP:
+                if visited.count((curr_coords[0] - 1, curr_coords[1])) == 0:
+                    items.append((curr_coords[0] - 1, curr_coords[1]))
+            if self.matrix[curr_coords[0]][curr_coords[1]].RIGHT:
+                if visited.count((curr_coords[0], curr_coords[1] + 1)) == 0:
+                    items.append((curr_coords[0], curr_coords[1] + 1))
+            if self.matrix[curr_coords[0]][curr_coords[1]].BOTTOM:
+                if visited.count((curr_coords[0] + 1, curr_coords[1])) == 0:
+                    items.append((curr_coords[0] + 1, curr_coords[1]))
+
+        dfs_nodes = [-1 for _ in range(self.width * self.height)]
+        dfs_nodes[self.convert_coords_to_id(coords_from)] = 1
+        # TODO: fix a bug, place where we start at isnt visited by default
+        visited = []
+        curr_coords = coords_from
+        update_items()
+        while len(items) != 0:
+            coords_to_check = items.pop()
+            curr_coords = coords_to_check
+            visited.append(coords_to_check)
+            dfs_nodes[self.convert_coords_to_id(coords_to_check)] = 1
+            update_items()
+        if dfs_nodes[self.convert_coords_to_id(coords_to)] == 1:
+            return True
+        return False
 
 
--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from enviroment.grid import Grid
 import config
 
 grid = Grid(config.HEIGHT, config.WIDTH)
+print("starting")
+grid.generate_matrix()
 while not grid.check_path(config.COORDS_FROM, config.COORDS_TO):
+    # TODO: fix the matrix, not redo it, since with big numbers it just dies
+    print("trying again")
     grid.generate_matrix()
 print(grid.matrix)
 
 
-# a = [(35000 if (usl or usl2) else -10000) for i in range(amount)]
 
